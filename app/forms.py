d60cdc05f52d37b5efc86460b878ebfc639368af
Leonardo Furtado <srleonardofurtado@gmail.com>
2021-01-13 17:51:34 -0300

    add Company Form

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Company
 from localflavor.br.br_states import STATE_CHOICES
 
 
@@ -6,3 +7,9 @@ class SearchModelForm(forms.Form):
     name = forms.CharField(max_length=100, required=False)
     uf = forms.ChoiceField(choices=STATE_CHOICES, required=False, initial={None: '----'})
     email = forms.EmailField(max_length=100, required=False)
+
+
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = ['name', 'uf', 'email', 'services']
