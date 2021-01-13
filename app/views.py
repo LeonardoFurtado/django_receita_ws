4b84816560468a3715766c8091cb112a6e9f1815
Leonardo Furtado <srleonardofurtado@gmail.com>
2021-01-13 18:13:26 -0300

    setup my views

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Company, ServiceOrder
-from .forms import SearchModelForm
+from .forms import SearchModelForm, CompanyForm, ServiceModelForm
 
 
 def list_companies(request):
@@ -16,3 +16,59 @@ def list_companies(request):
         companies = Company.objects.all()
 
     return render(request, 'companies.html', {'companies': companies, 'form': form})
+
+
+def create_company(request):
+    form = CompanyForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_companies')
+
+    return render(request, 'company-form.html', {'form': form})
+
+
+def update_company(request, id):
+    company = Company.objects.get(id=id)
+    form = CompanyForm(request.POST or None, instance=company)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_companies')
+
+    return render(request, 'company-form.html', {'form': form, 'company':company})
+
+
+def delete_company(request, id):
+    company = Company.objects.get(id=id)
+
+    if request.method == 'POST':
+        company.delete()
+        return redirect('list_companies')
+
+    return render(request, 'company-delete-confirm.html', {'company': company})
+
+
+def list_service_orders(request, company_id):
+    services_orders = ServiceOrder.objects.all().filter(company_id=company_id)
+    return render(request, 'service-orders.html', {'services_orders': services_orders})
+
+
+def create_service_order(request):
+    form = ServiceOrderModelForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_companies')
+
+    return render(request, 'service-orders-form.html', {'form': form})
+
+
+def create_service(request):
+    form = ServiceModelForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list_companies')
+
+    return render(request, 'service-form.html', {'form': form})
