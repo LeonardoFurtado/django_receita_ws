523990b7421524d32628a60a15d3dc79965af2f3
Leonardo Furtado <srleonardofurtado@gmail.com>
2021-01-13 21:06:29 -0300

    Create Base model
    The base model is a class with active, creation, and update date attributes that I like to use in all my models.

--- app/models.py
+++ app/models.py
@@ -2,14 +2,24 @@ from django.db import models
 from localflavor.br.br_states import STATE_CHOICES
 
 
-class Service(models.Model):
+class Base(models.Model):
+    """Base class used to load these attributes in all models."""
+    creation_date = models.DateField('Creation date', auto_now_add=True)
+    updated = models.DateField('Update date', auto_now_add=True)
+    is_active = models.BooleanField('active', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Service(Base):
     title = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
 
 
-class Company(models.Model):
+class Company(Base):
     name = models.CharField(max_length=100)
     uf = models.CharField(max_length=2, choices=STATE_CHOICES, null=True, blank=True)
     cnpj = models.CharField(max_length=14)
@@ -20,7 +30,7 @@ class Company(models.Model):
         return self.name
 
 
-class ServiceOrder(models.Model):
+class ServiceOrder(Base):
     title = models.CharField(max_length=100)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
